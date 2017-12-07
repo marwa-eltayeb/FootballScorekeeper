bceb4467ce32016dfae714822a4fe4697831dd03
Marwa-Said <marwa_eltayeb@yahoo.com>
2017-12-07 19:51:22 +0200

    Updated

--- app/src/main/java/com/example/marwa/footballscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/marwa/footballscorekeeper/MainActivity.java
@@ -11,42 +11,87 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Tracks the goals for Team A
+
+    /** Tracks the goals for Team A. */
     int goalTeamA = 0;
 
-    // Tracks the fouls for Team A
+    /** Tracks the fouls for Team A. */
     int foulTeamA = 0;
 
-    // Tracks the goals for Team B
+    /** Tracks the goals for Team B. */
     int goalTeamB = 0;
 
-    // Tracks the fouls for Team A
+    /** Tracks the fouls for Team A. */
     int foulTeamB = 0;
+
+    /** Shows the goals of team A. */
+    TextView goalA;
+
+    /** Shows the fouls of team A. */
+    TextView foulA;
+
+    /** Shows the goals of team B. */
+    TextView goalB;
+
+    /** Shows the fouls of team B. */
+    TextView foulB;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        /* Get references using findViewById() method */
+        goalA = (TextView) findViewById(R.id.team_a_goal);
+        foulA = (TextView) findViewById(R.id.team_a_foul);
+        goalB = (TextView) findViewById(R.id.team_b_goal);
+        foulB = (TextView) findViewById(R.id.team_b_foul);
+
+
+        /* Gets the goals and the fouls after the device is rotated. */
+        if (savedInstanceState != null) {
+            goalTeamA = savedInstanceState.getInt("goalTeamA");
+            foulTeamA = savedInstanceState.getInt("foulTeamA");
+            goalTeamB = savedInstanceState.getInt("goalTeamB");
+            foulTeamB = savedInstanceState.getInt("foulTeamB");
+            displayGoalsForTeamA(goalTeamA);
+            displayFoulsForTeamA(foulTeamA);
+            displayGoalsForTeamB(goalTeamB);
+            displayFoulsForTeamB(foulTeamB);
+        }
+
+
     }
 
-
+    /**
+     * Increase the goals for Team A by 1 point.
+     */
     public void addGoalsForTeamA(View view) {
         goalTeamA = goalTeamA + 1;
         displayGoalsForTeamA(goalTeamA);
     }
 
+    /**
+     * Increase the fouls for Team A by 1 point.
+     */
     public void addFoulsForTeamA(View view) {
         foulTeamA = foulTeamA + 1;
         displayFoulsForTeamA(foulTeamA);
     }
 
+    /**
+     * Increase the goals for Team B by 1 point.
+     */
     public void addGoalsForTeamB(View view) {
         goalTeamB = goalTeamB + 1;
         displayGoalsForTeamB(goalTeamB);
     }
 
-
+    /**
+     * Increase the fouls for Team B by 1 point.
+     */
     public void addFoulsForTeamB(View view) {
         foulTeamB = foulTeamB + 1;
         displayFoulsForTeamB(foulTeamB);
@@ -57,16 +102,14 @@ public class MainActivity extends AppCompatActivity {
      * Displays the goals for Team A.
      */
     public void displayGoalsForTeamA(int goal) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_goal);
-        scoreView.setText(String.valueOf(goal));
+        goalA.setText(String.valueOf(goal));
     }
 
     /**
      * Displays the fouls for Team A.
      */
     public void displayFoulsForTeamA(int foul) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_foul);
-        scoreView.setText(String.valueOf(foul));
+        foulA.setText(String.valueOf(foul));
     }
 
 
@@ -74,20 +117,21 @@ public class MainActivity extends AppCompatActivity {
      * Displays the goals for Team B.
      */
     public void displayGoalsForTeamB(int goal) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_goal);
-        scoreView.setText(String.valueOf(goal));
+        goalB.setText(String.valueOf(goal));
     }
 
     /**
      * Displays the fouls for Team B.
      */
     public void displayFoulsForTeamB(int foul) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_foul);
-        scoreView.setText(String.valueOf(foul));
+        foulB.setText(String.valueOf(foul));
     }
 
 
-    public void resetScore(View view) {
+    /**
+     * Reset the goals and fouls of the game.
+     */
+    public void resetGame(View view) {
         goalTeamA = 0;
         foulTeamA = 0;
         goalTeamB = 0;
@@ -96,6 +140,18 @@ public class MainActivity extends AppCompatActivity {
         displayFoulsForTeamA(foulTeamA);
         displayGoalsForTeamB(goalTeamB);
         displayFoulsForTeamB(foulTeamB);
+    }
+
+
+    /**
+     * Saves the goals and fouls when the device is rotated.
+     */
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("goalTeamA", goalTeamA);
+        savedInstanceState.putInt("foulTeamA", foulTeamA);
+        savedInstanceState.putInt("goalTeamB", goalTeamB);
+        savedInstanceState.putInt("foulTeamB", foulTeamB);
     }
 
 
